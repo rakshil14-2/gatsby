b35fa5c3ddf29f27a2d8deabf17a92aa30e92899
capitalch <capitalch@gmail.com>
2019-05-19 15:06:05 +0530

    init

--- capital-chowringhee/src/components/footer/footer.tsx
+++ capital-chowringhee/src/components/footer/footer.tsx
@@ -1,9 +1,32 @@
-import { Link } from "gatsby"
-import React from "react"
+import React, { useState } from 'react';
+import settings from '../../settings.json';
+import styles from './footer.module.scss';
+import fetch from 'isomorphic-unfetch';
 
 function Footer() {
-    return <footer className="footer">
-        <span>&copy;</span> Kush Infotech
-  </footer>
+	const [ hitCount, setHitCount ] = useState(0);
+
+	const loadHitCount = async () => {
+    // console.log(settings.hitCountUrl);
+		const res = await fetch(settings.hitCountUrl);
+		const tempHitCount = await res.json();
+		if (tempHitCount && tempHitCount.hits) {
+			settings.hitCount = tempHitCount.hits;
+			setHitCount(tempHitCount.hits); //forced client side refresh of page
+		}
+	};
+
+	if (!settings.hitCount) {
+		loadHitCount();
+	}
+
+	return (
+		<footer className={styles.footer}>
+			<div>
+				<span className={styles.hits}>Hits:{settings.hitCount}</span>
+				<span>&copy;</span> Kush Infotech
+			</div>
+		</footer>
+	);
 }
-export default Footer
+export default Footer;
--- capital-chowringhee/src/components/header/header.js
+++ capital-chowringhee/src/components/header/header.js
@@ -1,59 +1,61 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link } from 'gatsby';
+import React from 'react';
 import styles from './header.module.scss';
 // import { useMetaData } from '../../hooks/site-metadata';
-import {imageProcessor} from "../../hooks/image-processor-hook"
+import { imageProcessor } from '../../hooks/image-processor-hook';
 
 const Header = () => {
-  // const { metaData } = useMetaData();
-  const temp = imageProcessor("logo","fixed");
-  return <div className={styles.container}>
-    
-    <div className={styles.brandLogo}>
-      <Link to="/"> 
-        {/* {metaData.title}  */}
-        {temp}
-      </Link> 
-      
-    </div>
-    
-    <div className={styles.menu}>
-    
-        <Link to='/' className={styles.link} activeClassName={styles.active} >Home</Link>
-        <Link to='/aboutUs/' className={styles.link} activeClassName={styles.active}>About</Link>
-        <Link to='/contacts/' className={styles.link} activeClassName={styles.active}>Contacts</Link>
-        <Link to='/service/' className={styles.link} activeClassName={styles.active}>Service</Link>
-        <Link to='/showcase/' className={styles.link} activeClassName={styles.active}>Showcase</Link>
-        <Link to='/shop/' className={styles.link} activeClassName={styles.active}>Shop</Link>
+	// const { metaData } = useMetaData();
+  const temp = imageProcessor('logo', 'fixed');
+  // console.log('header traversed');
+	return (
+		<div className={styles.container}>
+			<div className={styles.brandLogo}>
+				<Link to="/">
+					{/* {metaData.title}  */}
+					{temp}
+				</Link>
+			</div>
 
-    </div>
+			<div className={styles.menu}>
+				<Link to="/" className={styles.link} activeClassName={styles.active}>
+					Home
+				</Link>
+				<Link to="/aboutUs/" className={styles.link} activeClassName={styles.active}>
+					About
+				</Link>
+				<Link to="/contacts/" className={styles.link} activeClassName={styles.active}>
+					Contacts
+				</Link>
+				<Link to="/service/" className={styles.link} activeClassName={styles.active}>
+					Service
+				</Link>
+				<Link to="/showcase/" className={styles.link} activeClassName={styles.active}>
+					Showcase
+				</Link>
+				<Link to="/shop/" className={styles.link} activeClassName={styles.active}>
+					Shop
+				</Link>
+			</div>
 
-    <div className={styles.brandName}>
-      {/* <Link to='/'>Login</Link> */}
-      Capital Chowringhee Pvt. Ltd.
-    </div>
-  </div>
-  
-}
-export default Header
+			<div className={styles.brandName}>
+				{/* <Link to='/'>Login</Link> */}
+				Capital Chowringhee Pvt. Ltd.
+			</div>
+		</div>
+	);
+};
+export default Header;
 
-
-
-
-
-
-
-
-
-
-
-//   <header className={styles.header}>
-  //     <h2 className={styles.headerLeft}>
-  //     <Link
-  //       to="/">
-  //       {metaData.title}
-  //     </Link>
-  //   </h2>
-  //   <HeaderMain></HeaderMain>
-  //   <HeaderRight></HeaderRight>
-  // </header>
+/*
+  <header className={styles.header}>
+      <h2 className={styles.headerLeft}>
+      <Link
+        to="/">
+        {metaData.title}
+      </Link>
+    </h2>
+    <HeaderMain></HeaderMain>
+    <HeaderRight></HeaderRight>
+  </header>
+*/
