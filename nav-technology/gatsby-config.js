4854afe3d901dafbfc62cf32ce57c47478e7a318
capitalch <capitalch@gmail.com>
2019-07-23 15:57:09 +0530

    init

--- nav-technology/gatsby-config.js
+++ nav-technology/gatsby-config.js
@@ -1,9 +1,36 @@
 module.exports = {
+  // siteMetadata: {
+  //   title: `Nav Technology`,
+  //   description: `Nav Technology is a Sony authorized service center in Kolkata that provides state of art Sony services at reasonable price`,
+  //   author: `Kush Infotech`,
+  //   siteUrl:'https://navtechnology.in'
+  // },
   siteMetadata: {
-    title: `Nav Technology`,
-    description: `Nav Technology is a Sony authorized service center in Kolkata that provides state of art Sony services at reasonable price`,
-    author: `Kush Infotech`,
-    siteUrl:'https://navtechnology.in'
+    title: 'Nav Technology Pvt Ltd, Sony Service Center',
+    description: `
+      Nav Technology is Sony authorized service center in Bagmari, Kankurgachi, North Kolkata which provides specialized repairs and services for Sony products.
+    `,
+    url: 'https://navtechnology.in',
+    image: 'https://navtechnology.in/11.jpg',
+    author: {
+      name: 'Sushant Agrawal',
+      minibio: `Sushant is software consultant in state of the art software technologies like React.js, Angular.js, Node.js, PostgreSql and many more.`,
+    },
+    organization: {
+      name: 'Nav technology Pvt Ltd',
+      url: 'https://navtechnology.in',
+      logo: 'https://navtechnology.in/logo.png',
+    },
+    social: {
+      twitter: '@nav_technology',
+      fbAppID: 'https://www.facebook.com/Nav-Technology-Pvt-Ltd-2856558957704521',
+    },
+    categories: [
+      {
+        slug: 'Service',
+        name: 'Sony Service Center',
+      },
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- nav-technology/src/components/seo.js
+++ nav-technology/src/components/seo.js
@@ -1,14 +1,8 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import SchemaOrg from './schema-org'
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
@@ -18,27 +12,83 @@ function SEO({ description, lang, meta, title }) {
           siteMetadata {
             title
             description
-            author
+            url
+            image
+            author {
+              name
+            }
+            organization {
+              name
+              url
+              logo
+            }
+            social {
+              twitter
+              fbAppID
+            }
           }
         }
       }
     `
   )
-
-// <Helmet>
-    //   <title>{title}</title>
-    //   <meta></meta>
-    //   <meta></meta>
-    // </Helmet>
-
-  const metaDescription = description || site.siteMetadata.description
-
+  // const metaDescription = description || site.siteMetadata.description
+const {title, description, url, image, author, organization, social } = site.siteMetadata;
   return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name='description'></meta>
-    </Helmet>
-    // <Helmet
+    <>
+      <Helmet>
+        {/* General tags */}
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="image" content={image} />
+        <link rel="canonical" href={url} />
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={url} />
+        {/* {isBlogPost ? <meta property="og:type" content="article" /> : null} */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+        <meta property="fb:app_id" content={seo.social.fbAppID} />
+
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={seo.social.twitter} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+      </Helmet>
+      <SchemaOrg
+              isBlogPost={false}
+              url={url}
+              title={title}
+              image={image}
+              description={description}
+              // datePublished={datePublished}
+              siteUrl={url}
+              author={author}
+              organization={organization}
+              defaultTitle={title} />
+    </>
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
+
+/*
+// <Helmet
     //   htmlAttributes={{
     //     lang,
     //   }}
@@ -79,20 +129,4 @@ function SEO({ description, lang, meta, title }) {
     //     },
     //   ].concat(meta)}
     // />
-  )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+*/
